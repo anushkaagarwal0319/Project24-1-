ba624426016f1c0b00ea860349e9477a7e22efe2
anushkaagarwal0319 <72772476+anushkaagarwal0319@users.noreply.github.com>
2021-01-20 11:50:37 -0500

    Add files via upload

---
+++ plane.js
@@ -0,0 +1,17 @@
+class Plane {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: true
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = 800;
+      this.height = 20;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("brown");
+      rect(pos.x, pos.y, this.width, this.height);
+    }
+  };
